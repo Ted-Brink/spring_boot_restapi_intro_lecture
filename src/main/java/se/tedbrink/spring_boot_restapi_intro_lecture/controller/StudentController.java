728b87b1eb5f3c7e7b01c60f21aa2e45d9c8b9e8
Ted-Brink <hexasus@gmail.com>
2021-10-13 18:04:29 +0200

    update

--- src/main/java/se/tedbrink/spring_boot_restapi_intro_lecture/controller/StudentController.java
+++ src/main/java/se/tedbrink/spring_boot_restapi_intro_lecture/controller/StudentController.java
@@ -2,22 +2,20 @@ package se.tedbrink.spring_boot_restapi_intro_lecture.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import se.tedbrink.spring_boot_restapi_intro_lecture.model.Student;
 
+import java.net.URI;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class StudentController {
 
     private List<Student> students = new ArrayList<>(
             Arrays.asList(
-                    new Student("1234", "kalle", "Anka", LocalDate.parse("1952-01-01"), "1952-01-01-9999", "Ankeborg"),
-                    new Student("98456", "janne", "Långben", LocalDate.parse("1954-01-01"), "1954-01-01-1234", "Ankeborg")
+                    new Student("1234", "Kalle", "Anka", LocalDate.parse("1952-01-01"), "1952-01-01-9999", "Ankeborg"),
+                    new Student("98456", "Janne", "Långben", LocalDate.parse("1954-01-01"), "1954-01-01-1234", "Ankeborg")
             )
     );
 
@@ -26,12 +24,98 @@ public class StudentController {
         return ResponseEntity.status(HttpStatus.valueOf(200)).body(students.get(0));
     }
 
-    @GetMapping("/api/students")
-    public ResponseEntity<List<Student>> findAll() {
+//    @GetMapping("/api/students") ////// Kan inte ha mer än en metod för findAll, genericFind() har defaultValue= "all"
+//    public ResponseEntity<List<Student>> findAll() {
+//
+//        if (students.isEmpty()) {
+//            return ResponseEntity.notFound().build();
+//        }
+//        return ResponseEntity.ok(students);
+//    }
 
-        if (students.isEmpty()) {
-            return ResponseEntity.notFound().build();
+    @PostMapping("/api/students")
+    public ResponseEntity<Student> create(@RequestBody Student student) {
+        students.add(student);
+        return  ResponseEntity.created(URI.create("localhost:8080/api/student/" + student.getStudentId())).body(student);
+    }
+
+    @GetMapping("api/students/{id}")
+    public ResponseEntity<Student> findById(@PathVariable("id") String studentId) {
+
+        Optional<Student> foundStudent = students.stream()
+                .filter( student -> student.getStudentId().equals(studentId))
+                .findFirst();
+
+        return ResponseEntity.ok(foundStudent.get());
+    }
+
+    @GetMapping("/api/students")
+    public ResponseEntity<?> genericFind(
+        @RequestParam(name = "type", defaultValue = "all") String type,
+        @RequestParam(name = "value", defaultValue = "all") String value
+    ){
+        switch (type.toLowerCase().trim()) {
+            case "all":
+                return ResponseEntity.ok(students);
+            case "firstname":
+                return ResponseEntity.ok(students.stream().filter(student -> student.getFirstName().equals(value)));
+            case "id":
+                return ResponseEntity.ok(students.stream().filter(student -> student.getStudentId().equals(value)).findFirst());
+            default:
+                return ResponseEntity.badRequest().body("Invalid Type");
         }
-        return ResponseEntity.ok(students);
+    }
+
+    @PutMapping("/api/students/{id}")
+    public ResponseEntity<Student> update(@PathVariable("id") String studentId,
+                                          @RequestBody Student student) {
+        if(studentId.equals(student.getStudentId())) {
+
+            Student original = students.stream().filter(s -> student.getStudentId().equals(studentId)).findFirst().get();
+//            students.remove(original);
+
+            original.setFirstName(student.getFirstName());
+            original.setLastName(student.getLastName());
+            original.setBirthDate(student.getBirthDate());
+            original.setPsn(student.getPsn());
+            original.setAddress(student.getAddress());
+
+//            students.add(original);
+
+            return ResponseEntity.ok().body(original);
+        }else
+            return ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/api/students/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") String studentId) {
+        Student original = students.stream().filter(s -> s.getStudentId().equals(studentId)).findFirst().get();
+        boolean wasRemoved = students.remove(original);
+
+        if (wasRemoved) {
+            return ResponseEntity.ok().build();
+        }else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
